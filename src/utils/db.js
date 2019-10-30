1131894662537912c9e3466e14324e74a6d85a1b
Ryan White <ryanjwhite10@gmail.com>
2019-10-30 07:21:49 -0700

    Add db connection util

---
+++ src/utils/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+import options from '../config';
+
+export const connect = (url = options.dbUrl, opts = {}) => {
+  return mongoose.connect(url, {
+    ...opts,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+  });
+};
