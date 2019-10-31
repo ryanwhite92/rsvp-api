6ac7e7a50ec89adf086935dcd55b70e042e0d7f0
Ryan White <ryanjwhite10@gmail.com>
2019-10-31 07:24:29 -0700

    Add body parsing and logging middleware

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 'use strict';
 
 import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 import config from './config';
 import { connect } from './utils/db';
 
 const app = express();
+
+// parses application/json and application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => res.send('hello world'));
 
