c1043746d9aadf6874e9082530fd4eba3fd39a04
Ryan White <ryanjwhite10@gmail.com>
2019-11-01 12:57:31 -0700

    Add generic getOne crud method

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,3 +1,23 @@
+export const getOne = model => async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const doc = await model
+      .findById(id)
+      .lean()
+      .exec();
+
+    if (!doc) {
+      return res.status(400).end();
+    }
+
+    return res.status(200).json({ data: doc });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).end();
+  }
+};
+
 export const createOne = model => async (req, res) => {
   try {
     const doc = await model.create({ ...req.body });
@@ -12,3 +32,8 @@ export const createOne = model => async (req, res) => {
     return res.status(500).end();
   }
 };
+
+export const crudControllers = model => ({
+  getOne: getOne(model),
+  createOne: createOne(model)
+});
