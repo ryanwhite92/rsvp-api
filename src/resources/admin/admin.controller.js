4ec190816a480610a97fca590682c04b5a688e49
Ryan White <ryanjwhite10@gmail.com>
2019-11-04 08:07:17 -0800

    Create admin signin controller

---
+++ src/resources/admin/admin.controller.js
@@ -0,0 +1,31 @@
+import { newToken } from '../../utils/auth';
+import { Admin } from '../resources/admin/admin.model';
+
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).send({ message: 'Email and password required' });
+  }
+
+  const invalidMessage = 'Invalid username and password';
+  try {
+    const admin = await Admin.findOne({ email })
+      .select('email password')
+      .exec();
+
+    if (!admin) {
+      return res.status(401).send({ message: invalidMessage });
+    }
+
+    const passwordMatch = await admin.checkPassword(password);
+    if (!passwordMatch) {
+      return res.status(401).send({ message: invalidMessage });
+    }
+
+    const token = auth.newToken(admin);
+    return res.status(201).send({ token });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).end();
+  }
+};
