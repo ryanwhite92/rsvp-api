cb50e4c23dd30d37f03164dea9d31cae9026bf15
Ryan White <ryanjwhite10@gmail.com>
2019-11-05 06:56:03 -0800

    Add verifyToken method to verify jwts

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,8 +1,20 @@
 import config from '../config';
 import jwt from 'jsonwebtoken';
+import { rejects } from 'assert';
 
 export const newToken = user => {
   return jwt.sign(user, config.secrets.jwt, {
     expiresIn: config.secrets.jwtExp
+  });
+};
+
+export const verifyToken = token => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, config.secrets.jwt, (err, payload) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(payload);
+    });
   });
 };
