483c6734fe80449e883eb50965b538fa30fcc430
Ryan White <ryanjwhite10@gmail.com>
2019-11-21 07:32:09 -0800

    Allow guests to update rsvpStatus of their plusOne(s)

--- src/resources/guest/guest.controller.js
+++ src/resources/guest/guest.controller.js
@@ -4,15 +4,36 @@ import { crudControllers } from '../../utils/crud';
 
 const updateRsvp = async (req, res) => {
   const { id } = req.params;
-  const { rsvpStatus } = req.body;
+  const { rsvpStatus, plusOnes = [] } = req.body;
 
   try {
-    // Update guest if successful
+    const guest = await Guest.findById(id)
+      .lean()
+      .exec();
+
+    if (!guest) {
+      return res.status(400).end();
+    }
+
+    // Update rsvpStatus for existing plusOnes
+    // Todo: Add logging if user tries to add additional plusOne(s)
+    const updatedPlusOnes = guest.plusOnes.map(guestPlusOne => {
+      plusOnes.forEach(plusOne => {
+        if (
+          guestPlusOne.name == plusOne.name &&
+          typeof plusOne.rsvpStatus == 'boolean'
+        ) {
+          guestPlusOne.rsvpStatus = plusOne.rsvpStatus;
+        }
+      });
+      return guestPlusOne;
+    });
+
     const updatedGuest = await Guest.findByIdAndUpdate(
       id,
       {
         rsvpStatus,
-        plusOnes
+        plusOnes: updatedPlusOnes
       },
       { new: true }
     )
