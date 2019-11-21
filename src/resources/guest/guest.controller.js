d4b30081e92e316c3394a0f721622b78ee13d245
Ryan White <ryanjwhite10@gmail.com>
2019-11-20 19:51:55 -0800

    Add function to update guest rsvp status

--- src/resources/guest/guest.controller.js
+++ src/resources/guest/guest.controller.js
@@ -2,7 +2,32 @@ import { Guest } from './guest.model';
 import { signin } from '../../utils/auth';
 import { crudControllers } from '../../utils/crud';
 
+const updateRsvp = async (req, res) => {
+  const { id } = req.params;
+  const { rsvpStatus } = req.body;
+
+  try {
+    // Update guest if successful
+    const updatedGuest = await Guest.findByIdAndUpdate(
+      id,
+      {
+        rsvpStatus,
+        plusOnes
+      },
+      { new: true }
+    )
+      .lean()
+      .exec();
+
+    return res.status(200).json({ data: updatedGuest });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).end();
+  }
+};
+
 export default {
   ...crudControllers(Guest),
-  signin: signin(Guest)
+  signin: signin(Guest),
+  updateRsvp
 };
