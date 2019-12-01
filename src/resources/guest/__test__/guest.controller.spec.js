5e5b114aefd3cbb9c284d79c9ba293fc8d733ec5
Ryan White <ryanjwhite10@gmail.com>
2019-12-01 12:16:38 -0800

    Test updating only existing plus ones

--- src/resources/guest/__test__/guest.controller.spec.js
+++ src/resources/guest/__test__/guest.controller.spec.js
@@ -99,6 +99,51 @@ describe('guest controllers', () => {
       await controllers.updateRsvp(req, res);
     });
 
+    test('only update existing plus one rsvp status', async () => {
+      expect.assertions(4);
+
+      const mockGuest = await Guest.create({
+        firstName: 'Test First',
+        lastName: 'Test Last',
+        plusOnes: [{ name: 'Plus One' }, { name: 'Plus Two' }],
+        contact: { method: 'email' }
+      });
+
+      const req = {
+        params: {
+          id: mockGuest._id
+        },
+        body: {
+          rsvpStatus: true,
+          plusOnes: [
+            { name: 'Plus One', rsvpStatus: true },
+            { name: 'Plus Three', rsvpStatus: true }
+          ]
+        }
+      };
+
+      const expectedResult = [
+        { name: 'Plus One', rsvpStatus: true },
+        { name: 'Plus Two', rsvpStatus: false }
+      ];
+
+      const res = {
+        status(status) {
+          expect(status).toBe(200);
+          return this;
+        },
+        json(result) {
+          expect(`${result.data._id}`).toBe(`${mockGuest._id}`);
+          expect(result.data.rsvpStatus).toBe(true);
+          expect(result.data.plusOnes).toEqual(
+            expect.arrayContaining(expectedResult)
+          );
+        }
+      };
+
+      await controllers.updateRsvp(req, res);
+    });
+
     test("404 if guest isn't found (invalid id)", async () => {
       expect.assertions(2);
 
