e46e46b81c7250f28c5b0e48209ceaaf1e2898bd
Ryan White <ryanjwhite10@gmail.com>
2019-12-01 15:29:47 -0800

    Add admin router tests

---
+++ src/resources/admin/__test__/admin.router.spec.js
@@ -0,0 +1,20 @@
+import router from '../admin.router';
+
+describe('admin router', () => {
+  test('admin routes exist', () => {
+    const routes = [
+      { path: '/', method: 'get' },
+      { path: '/:id', method: 'get' },
+      { path: '/signin', method: 'post' },
+      { path: '/signup', method: 'post' }
+    ];
+
+    routes.forEach(route => {
+      const isMatch = router.stack.find(
+        s =>
+          s.route && s.route.path == route.path && s.route.methods[route.method]
+      );
+      expect(isMatch).toBeTruthy();
+    });
+  });
+});
