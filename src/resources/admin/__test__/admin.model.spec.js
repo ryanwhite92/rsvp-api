20ac953ace91d28c6cd89dd3baf4ee2acc439085
Ryan White <ryanjwhite10@gmail.com>
2019-12-02 06:57:56 -0800

    Test admin schema

---
+++ src/resources/admin/__test__/admin.model.spec.js
@@ -0,0 +1,32 @@
+import { Admin } from '../admin.model';
+
+describe('admin model', () => {
+  describe('schema', () => {
+    test('email', () => {
+      const email = Admin.schema.obj.email;
+      expect(email).toEqual({
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+      });
+    });
+
+    test('password', () => {
+      const password = Admin.schema.obj.password;
+      expect(password).toEqual({
+        type: String,
+        required: true
+      });
+    });
+
+    test('role', () => {
+      const role = Admin.schema.obj.role;
+      expect(role).toEqual({
+        type: String,
+        default: 'admin',
+        enum: ['admin']
+      });
+    });
+  });
+});
